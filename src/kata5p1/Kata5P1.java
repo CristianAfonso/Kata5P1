1916d79de27a24f6b9737d286bb7fbd5624eec76
can97 <can97@DESKTOP-ETFFE43>
2021-01-13 19:51:14 +0000

    Kata5P1 podemos conectarnos a nuestra BD y realizar una visualización por consola de los valores en esta.

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -3,6 +3,7 @@ package kata5p1;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Kata5P1 {
     
@@ -11,6 +12,7 @@ public class Kata5P1 {
         connection();
         SelectApp app = new SelectApp();
         app.selectAll();
+        createNewTable();
     }
     
     
@@ -30,6 +32,22 @@ public class Kata5P1 {
                 System.out.println(ex.getMessage());
             }
         }
+    }
+    
+    //Crear una tabla en la BD para la v3:
+    public static void createNewTable(){
+        String url  =    "jdbc:sqlite:KATA5.db";
+        String sql  =    "CREATE TABLE IF NOT EXISTS EMAIL (\n"
+                        + " id integer PRIMARY KEY AUTOINCREMENT, \n"
+                        + " mail text NOT NULL);";
+        try (Connection con = DriverManager.getConnection(url);
+                Statement stmt = con.createStatement()){
+            stmt.execute(sql);
+            
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+                
     }
     
 }
